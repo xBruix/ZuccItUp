87b220a4c4bee7caa0d9808154de6c305f2c9081
Surya6704 <surya4balram@outlook.com>
2026-03-24 12:42:29 -0700

    added calls to functions and inputs for customer.py from order.py

--- src/customer.py
+++ src/customer.py
@@ -1,3 +1,5 @@
+# Authors: Surya Balram
+
 import notification     #importing member functions from notification.py
 import order            #importing member functions from order.py
 import menu             #importing member functions from menu.py
@@ -8,14 +10,33 @@ import menu             #importing member functions from menu.py
 
 
 ##order.py
-#placeOrder
+#place_Order
+def customer_place_order(self):
+    bool message = place_Order(self)     #function to place an order
+
 #viewOrder
-#seeAllTimes
-#addToCart
+def customer_view_order(self):
+    dict message = viewOrder(self)       #function to view an order
+
+#seeAllTimes = TBD
+
+#addToCart = input
+def customer_add_to_cart(self):
+    menu_Item = input("Enter the menu item you want to add to your cart: ").strip()
+    quantity = input("Enter the quantity: ").strip()
+    add_to_cart(self, menu_Item, quantity)     #function to add items to cart
+
 #viewCart
-#enterLocation
-#createCart
+def customer_view_cart(self):
+    dict message = view_cart(self)         #function to view the cart
+
+#enterLocation = input
+
+
+
 
 ##menu.py
-#viewMenu
+#viewMenu = input
+
+
 #viewAllMenus
