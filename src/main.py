1cae639f55bc12f59ab9fb4a33d1db4c690d8912
Chickenlord7321 <brick.cuppy@gmail.com>
2026-03-28 19:44:22 -0700

    Changed input_str to use regex, added template code for choosing a vendor/menu

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from order import Order
 from user import User, Customer, DeliveryAgent, Vendor
 from getpass import getpass		# for typing password without showing letters
 import sys						# to exit program
+import re						# for regex
 
 
 # FOR TESTING ONLY.
@@ -22,32 +23,34 @@ def check_logout_or_quit(answer: str) -> str:
 		server.disconnect()
 		sys.exit(0)
 	elif answer.lower() == "logout":
-		# Not sure what to do here yet
+		user.logout()
+		print("You have successfully logged out!")
+		# Not sure what should happen now?
 		return answer
 	else:
 		return answer
 
-def input_str(msg: str, valid_answers: list[str] = None) -> str:
+def input_str(msg: str, regex_pattern: str = None) -> str:
 	"""
 	Gets string input and checks if the user typed "quit" or "logout".
 	:param msg: The message you want to display to the user
-	:param valid_answers: (optional) A list of acceptable answers. You do not need to include "quit" or "logout" in this list.
+	:param regex_pattern: (optional) A regex string to match the user input to.
 	:return: the user's input
 	"""
 
 	# If any string input is acceptable, simply return the user's answer after checking for quit or logout
-	if valid_answers is None:
+	if regex_pattern is None:
 		return check_logout_or_quit(input(msg))
 
-	# Else, we need to check if the answer is valid.
-	# Make sure that all the strings in valid_answers are in lower case.
-	valid_answers = [s.lower() for s in valid_answers]
-	while True:
+	# Else, we need to check if the answer is valid -- i.e. that it matches regex_pattern
+	matching = True
+	answer = ""
+	while not matching:
 		answer = check_logout_or_quit(input(msg))
-		if answer.lower() in valid_answers:
-			return answer
-		else:
-			print("Sorry, that was not a valid answer. Please try again")
+		matching = re.match(regex_pattern, answer)
+		if not matching:
+			print("Sorry, that was not a valid answer. Please try again.")
+	return answer
 
 # Stackoverflow to explain sys.maxsize:
 # https://stackoverflow.com/questions/7604966/maximum-and-minimum-values-for-ints
@@ -76,10 +79,9 @@ def input_int(msg: str, minimum: int = -sys.maxsize - 1, maximum: int = sys.maxs
 def login_or_signup():
 	not_signed_in = True
 	while not_signed_in:
-		print("Welcome to Zucc It Up!")
-		option = input_int("Please select an option:\n1. Login\n2. Signup\n> ", 1, 2)
+		signin_option = input_int("Please select an option:\n1. Login (type 1)\n2. Signup (type 2)\n> ", 1, 2)
 
-		if option == 1:		# Login
+		if signin_option == 1:		# Login
 			your_viu_id = input_str("Please enter your VIU ID number\n> ")
 			your_password = getpass("Please enter your password\n> ")
 			not_signed_in = not user.login(your_viu_id, your_password)
@@ -87,8 +89,8 @@ def login_or_signup():
 				print("Sorry, those login details were incorrect. Please try again.")
 		else:				# Signup
 			print("Let's get you started!")
-			option = input_int("Choose a role:\n1. Customer\n2. Delivery Agent", 1, 2)
-			your_role = "Customer" if option == 1 else "Agent"
+			signin_option = input_int("Choose a role:\n1. Customer\n2. Delivery Agent", 1, 2)
+			your_role = "Customer" if signin_option == 1 else "Agent"
 			your_viu_id = input_str("Please enter your VIU ID number\n> ")
 			your_password = getpass("Please enter a password\n> ")
 			your_name = input_str("Please enter your name\n> ")
@@ -98,11 +100,31 @@ def login_or_signup():
 				print("Sorry, something went wrong. Please try again.")
 # end login_or_signup
 
-# MAIN LOOP
-if __name__ == "__main__":
-	login_or_signup()
-	print("Goodbye!")
-	print("""
+
+# MAIN LOOP GOES HERE
+print("Welcome to Zucc It Up!")
+print("At any time, you can type 'quit' to close the program, or type 'logout' to logout of your account.")
+login_or_signup()
+
+if user.get_role() == "Customer":
+	# TODO: Enter Location (user location)
+	your_building = input_str("Enter your building number\n> ", "^[1-4]\\d\\d$")
+	your_room = input_str("Enter your room number\n> ", "^[1-5]\\d\\d\\w?$")
+
+	# TODO: List options
+	customer_option = input_str("What do you want to do?\n1. Create an Order \n2. View Your Cart")
+	if customer_option == 1:	# Create an Order
+		# TODO: List vendors
+		# TODO: Customer selects vendor
+		# TODO: List menus for that vendor
+		# TODO: Customer selects a menu
+		# TODO: List menu items from that menu
+		pass
+	else:						# View your cart
+		pass
+
+print("Goodbye!")
+print("""
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⣤⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⣠⡶⠒⠒⠶⣄⣠⡴⠚⠉⠁⠀⠀⠀⠀⠀⠉⠙⠳⢦⡀⠀⠀⠀⠀⠀⠀
 ⢠⡏⠀⠀⠀⠀⠘⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢧⡀⠀⠀⠀⠀
@@ -117,4 +139,4 @@ if __name__ == "__main__":
 ⢠⡏⠘⠋⠀⠀⠀⠈⠑⠦⣄⣀⠀⠀⠀⠀⠀⣀⡠⠔⠋⠀⠀⠀⠈⠛⠃⢻
 ⠈⠷⣤⣀⣀⣀⣀⣀⣀⣀⣀⣤⡽⠟⠛⠿⣭⣄⣀⣀⣀⣀⣀⣀⣀⣀⣤⠞
 ⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠉⠀⠀⠀
-	""")
+""")
